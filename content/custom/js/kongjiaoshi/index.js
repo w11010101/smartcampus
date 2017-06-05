ff399ba9abad18b894b44f03b1e849e123c76516
w11010101 <w11010101@hotmail.com>
2017-06-05 17:52:25 +0800

    0

--- content/custom/js/kongjiaoshi/index.js
+++ content/custom/js/kongjiaoshi/index.js
@@ -1,6 +1,9 @@
+$(function () {
+  FastClick.attach(document.body);
+});
+
 var value;
 $(".smart-list-item div[set-type]").on("click", function () {
- 
   var that = $(this);
   var type = $(this).attr("set-type");
   var setArr = [];
@@ -27,13 +30,13 @@ $(".smart-list-item div[set-type]").on("click", function () {
       "campusid": "4",
       "campusname": "北校区"
     }];
-    for(var i in setArr){
+    for (var i in setArr) {
       newArr.push({
-        key:setArr[i].campusid || setArr[i].buildid,
-        name:setArr[i].campusname || setArr[i].buildname
+        key: setArr[i].campusid || setArr[i].buildid,
+        name: setArr[i].campusname || setArr[i].buildname
       })
     }
-    
+
     break;
   case "floor":
     // 选择楼栋
@@ -54,10 +57,10 @@ $(".smart-list-item div[set-type]").on("click", function () {
       "buildid": "14",
       "buildname": "东区4号楼"
     }];
-    for(var i in setArr){
+    for (var i in setArr) {
       newArr.push({
-        key:setArr[i].campusid || setArr[i].buildid,
-        name:setArr[i].campusname || setArr[i].buildname
+        key: setArr[i].campusid || setArr[i].buildid,
+        name: setArr[i].campusname || setArr[i].buildname
       })
     }
     break;
@@ -66,15 +69,15 @@ $(".smart-list-item div[set-type]").on("click", function () {
   campus.popup(newArr, function (data) {
     console.log(data);
     // 添加自定义属性
-    that.find(".mart-list-layer-2").html('<h3 key="'+data.key+'">' + data.name + '</h3>');
+    that.find(".mart-list-layer-2").html('<h3 key="' + data.key + '">' + data.name + '</h3>');
     that.attr("select", true);
     // 删除重复的蓝色标签
     $.each($(".campus-index-screen-title em"), function (i, e) {
-      if ($(e).attr("label-type") == type) {
-        $(e).remove();
-      }
-    })
-    // 添加蓝色标签
+        if ($(e).attr("label-type") == type) {
+          $(e).remove();
+        }
+      })
+      // 添加蓝色标签
     $(".campus-index-screen-title").prepend('<em class="campus-index-screen-label" label-type="' + type + '">' + data.name + '</em>');
     // 改变确认按钮颜色
     if ($("*[select=true]").length == 3) {
@@ -83,6 +86,99 @@ $(".smart-list-item div[set-type]").on("click", function () {
   });
 })
 
-$(".smart-container-sure-btn").on("click",function(){
-  
+$(".smart-container-sure-btn").on("click", function () {
+
 })
+
+// 滑动效果
+var startX, startY, endX, endY;
+$(window).on("touchstart", function (e) {
+
+  startY = e.changedTouches[0].pageY;
+  startX = e.changedTouches[0].pageX;
+}).on("touchmove", function (e) {
+
+  endY = e.changedTouches[0].pageY;
+  endX = e.changedTouches[0].pageX;
+}).on("touchend", function (e) {
+
+  endY = e.changedTouches[0].pageY;
+  endX = e.changedTouches[0].pageX;
+
+  console.log(endY - startY);
+  var Y = endY - startY;
+  var X = endX - startX;
+  if (X == 0) {
+    // 点击无效果
+    console.log("点击无效果");
+  } else if (Y > 0 && Math.abs(Y) > 0) {
+    // 向下
+    console.log("向右");
+  } else if (Y < 0 && Math.abs(Y) > 0) {
+    // 向上
+    console.log("向左");
+  }
+
+  if (Y == 0) {
+    // 点击无效果
+    console.log("点击无效果");
+  } else if (Y > 0 && Math.abs(Y) > 0) {
+    // 向下
+    console.log("向下");
+  } else if (Y < 0 && Math.abs(Y) > 0) {
+    // 向上
+    console.log("向上");
+  }
+})
+
+$.fn.swipe = function (option) {
+  // console.log(value);
+  var defaults  = {
+    swipeUp : function(){
+      console.log("swipeUp");
+    },
+    swipeDown : function(){
+      console.log("swipeDown");
+    },
+    swipeLeft : function(){
+      console.log("swipeLeft");
+    },
+    swipeRight : function(){
+      console.log("swipeRight");
+    }
+
+  }
+
+  var obj = $.extend(defaults,option);
+  // this.swipeUp = function(){
+  //   console.log("swipeUp");
+  // }
+
+  // this.swipeDown = function(){
+  //   console.log("swipeDown");
+  // }
+
+  // this.swipeLeft = function(){
+  //   console.log("swipeLeft");
+  // }
+
+  // this.swipeRight = function(){
+  //   console.log("swipeRight");
+  // }
+  // callback(obj);
+  return obj;
+}
+// console.log($(window).swipe({
+//   swipeUp:function(){
+//     console.log(0)
+//   }
+// }))
+$(window).swipe({
+  swipeUp: function () {
+    console.log("swipeDown");
+  },
+  swipeDown: function () {
+    console.log("swipeDown");
+  }
+})
+
