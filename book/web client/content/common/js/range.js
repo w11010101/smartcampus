98f1c1c631687aca8e9cc58a2b9455622aa3918c
w11010101 <w11010101@hotmail.com>
2018-04-04 18:15:10 +0800

    2018.4.3

--- book/web client/content/common/js/range.js
+++ book/web client/content/common/js/range.js
@@ -44,16 +44,19 @@ function getVidDur(){
   alert(myVideo.duration);
 } 
 $("video").on("mouseenter",function(){
-    videoTool.fadeIn(200);
+    // videoTool.fadeIn(200);
 });
 videoTool.on("mouseleave",function(){
-    // videoTool.fadeOut(200);
+    if(!myVideo.paused){
+        videoTool.stop().fadeOut(200);   
+    }
+    
 });
 $("video").on("click",function(){
-    videoTool.fadeIn(200);
+    videoTool.stop().fadeIn(200);
     setTimeout(function(){
         if(!myVideo.paused){
-            // videoTool.fadeOut(200);
+            // videoTool.stop().fadeOut(200);
         }
     },3000);
 })
@@ -94,13 +97,101 @@ function changeFn(obj){
     var currentTime = (myVideo.duration*(parseFloat(obj.value)/100)).toFixed(2);
     myVideo.currentTime = currentTime;
 }
+// ESC 退出全屏
+document.onkeydown = function(){
+    screenChangeEvent();
+}
+function browserVersion() {
+    var userAgent = navigator.userAgent,     
+        rMsie = /(msie\s|trident.*rv:)([\w.]+)/,     
+        rFirefox = /(firefox)\/([\w.]+)/,     
+        rOpera = /(opera).+version\/([\w.]+)/,     
+        rChrome = /(chrome)\/([\w.]+)/,     
+        rSafari = /version\/([\w.]+).*(safari)/;    
+    var browser;    
+    var version;    
+    var ua = userAgent.toLowerCase();    
+    function uaMatch(ua){    
+        var match = rMsie.exec(ua);    
+        if(match != null){    
+            return { browser : "IE", version : match[2] || "0" };    
+        }    
+        var match = rFirefox.exec(ua);    
+            if (match != null) {    
+            return { browser : match[1] || "", version : match[2] || "0" };    
+        }    
+        var match = rOpera.exec(ua);    
+            if (match != null) {    
+            return { browser : match[1] || "", version : match[2] || "0" };    
+        }    
+        var match = rChrome.exec(ua);    
+            if (match != null) {    
+            return { browser : match[1] || "", version : match[2] || "0" };    
+        }    
+        var match = rSafari.exec(ua);    
+            if (match != null) {    
+            return { browser : match[2] || "", version : match[1] || "0" };    
+        }    
+        if (match != null) {    
+            return { browser : "", version : "0" };    
+        }    
+    }    
+    var browserMatch = uaMatch(userAgent.toLowerCase());
+    if (browserMatch.browser){
+        browser = browserMatch.browser;
+        version = browserMatch.version;
+    }
+    return browser + version
+}
+screenChangeEvent();
+function screenChangeEvent(element) {
+    var _this = this;
+    var browserV = browserVersion();
+    if(browserV.indexOf('IE11') >= 0) {
+        document.onkeydown = function (e) {
+            var keyNum = window.event ? e.keyCode : e.which
+            if (keyNum === 27 && _this.isFull) {
+                // ie退出全屏   这里针对的是IE11
+                exitFullscreen($(".video")[0]);
+            }
+        }
+    } else if (browserV.indexOf('IE10') >= 0 || browserV.indexOf('IE9') >= 0) {
+        document.onkeydown = function (e) {
+            var keyNum = window.event ? e.keyCode : e.which
+            if (keyNum === 27 && _this.isFull) {
+                // ie退出全屏   这里针对的是IE10  9
+                // _this.exitFullscreenIE11L()
+            }
+        }
+    } else {
+        var eventList = ['webkitfullscreenchange', 'mozfullscreenchange', 'fullscreenchange', 'msfullscreenchange']
+        for(var i = 0; i < eventList.length; i++) {
+            document.addEventListener(eventList[i], function () {
+                // 全屏显示的网页元素
+                var fullscreenElement  = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement
+
+                // 判断网页是否处于全屏状态下
+                var isFullScreen = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen || document.msIsFullScreen
+
+                if (fullscreenElement) {
+                    console.log('全屏',)
+                    // _this.launchFullScreenStyle(_this.opt.ele);
+                    fullScreenStyle($(".video")[0]);
+                } else {
+                    console.log('不是全屏')
+                    exitFullscreen($(".video")[0]);
+                }
+            })
+        }
+    }
+}
 // 全屏
 var isFull = false;
 function fullScreen(){
     if(!isFull){
-        launchFullScreen($("video")[0]);
+        launchFullScreen($(".video")[0]);
     }else{
-        exitFullscreen();
+        exitFullscreen($(".video")[0]);
     }
     isFull = !isFull;
 }
@@ -115,9 +206,17 @@ function launchFullScreen(element) {
     } else if (element.msRequestFullscreen) {
         element.msRequestFullscreen();
     }
+    fullScreenStyle(element);
+}
+function fullScreenStyle(element){
+    $(element).css({
+        width:"100%",
+        height:"100%",
+    });
+    $(".full-screen").addClass("flex-screen");
 }
 // 退出全屏
-function exitFullscreen() {
+function exitFullscreen(element) {
     if (document.exitFullscreen) {
         document.exitFullscreen();
     } else if (document.mozCancelFullScreen) {
@@ -127,4 +226,12 @@ function exitFullscreen() {
     } else if (document.msExitFullscreen) {
         document.msExitFullscreen();
     }
+    refreshFullScreenStyle(element);
+}
+function refreshFullScreenStyle(element){
+    $(element).css({
+        width:"600px",
+        height:"340px",
+    });
+    $(".full-screen").removeClass("flex-screen");
 }
