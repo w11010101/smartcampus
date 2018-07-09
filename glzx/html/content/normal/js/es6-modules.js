971e834c1b4e63deaafd7f7cb0892aef37cafa49
w11010101 <w11010101@hotmail.com>
2018-07-09 17:02:55 +0800

    2018.7.9 整理文件夹

--- glzx/html/content/normal/js/es6-modules.js
+++
@@ -1,9 +0,0 @@
-var firstName = 'wangchi';
-var age = 27;
-function myFn(x,y){
-    this.run = function(){
-        console.log("run")
-    }
-    return x+y;
-}
-export {age,myFn};
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import _ form "lodash"
-function component(){
-    var element = document.createElement("div");
-    element.innerHTML = _.join(["hello","webpack"]);
-    return element;
-}
-document.body.appendChild(component());
